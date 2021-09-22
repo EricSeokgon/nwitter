3f5e011a11d75bb02409686428dbf87f268a1d40
Eric <leesk55@gmail.com>
2021-09-22 20:13:14 +0900

    Securing the Keys

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import * as firebase from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCos-etZ0tyN6K5DODQX3-D-E1YEhuclb4",
-    authDomain: "nwitter-a65ff.firebaseapp.com",
-    projectId: "nwitter-a65ff",
-    storageBucket: "nwitter-a65ff.appspot.com",
-    messagingSenderId: "899461319757",
-    appId: "1:899461319757:web:442591b2f7de2c29ca6004",
-    measurementId: "G-NJ6K29CKCG"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+    appId: process.env.REACT_APP_APP_ID,
 };
 
 export default firebase.initializeApp(firebaseConfig);
