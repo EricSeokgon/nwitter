57763629c2cc42196ec79f996e3c3ed59e68e5e3
Eirc <leesk55@gmail.com>
2021-09-23 14:05:22 +0900

    Recap

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,10 +1,9 @@
 import React, { useState } from "react";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
-import Auth from "../routes/Auth";
-import Home from "../routes/Home";
+import Auth from "routes/Auth";
+import Home from "routes/Home";
 
 const AppRouter = ({ isLoggedIn }) => {
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
     return (
         <Router>
             <Switch>
