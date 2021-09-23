200f1966e175f63616a2002cdd406e73f4b308f0
Eirc <leesk55@gmail.com>
2021-09-23 13:51:08 +0900

    Login Form part One

--- src/components/Router.js
+++ src/components/Router.js
@@ -3,7 +3,7 @@ import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Auth from "../routes/Auth";
 import Home from "../routes/Home";
 
-const AppRouter = () => {
+const AppRouter = ({ isLoggedIn }) => {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
     return (
         <Router>
